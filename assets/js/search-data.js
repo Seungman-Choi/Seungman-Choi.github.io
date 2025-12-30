6ae80f5d404f962bb395d8c7322a4728c37eda74
Seungman-Choi <87984418+Seungman-Choi@users.noreply.github.com>
2025-12-30 21:09:39 +0000

    Deploying to gh-pages from @ Seungman-Choi/Seungman-Choi.github.io@6122837c2ac2b17a59151d03680e47e0aa1b1328 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,8 +28,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-",
+          title: '',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/Announcement_0/";
